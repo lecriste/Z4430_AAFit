d3f1fc3e3bfea9a59fd5d4748ca1174ec5565470
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-07-07 18:25:42 +0200

    mMuMu

--- Test.cu
+++ Test.cu
@@ -179,7 +179,7 @@ int main(int argc, char** argv)
 
   TH1F* dataHisto = new TH1F("data","data",50,massKPi.lowerlimit,massKPi.upperlimit);
   // B^{0} -> psi(nS) #pi^{+} K^{-}
-  GooPdf* phaseSpace = new ThreeBodiesPsiPiK ("phasespace",&massKPi,&mBd,&mPion,&mKaon);
+  GooPdf* phaseSpace = new ThreeBodiesPsiPiK ("phasespace",&massKPi,&mBd,&mPion,&mKaon,&mMuMu);
   //cout <<"\nBdToMuMuPiK_PHSP.getVal() =\n" <<BdToMuMuPiK_PHSP->getVal() <<endl; return;
 
   fptype roll=0.0;
