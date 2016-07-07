231a58a67bdd2f771b77adc580a33180a5d2513b
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-07-07 18:50:57 +0200

    1.

--- Test.cu
+++ Test.cu
@@ -148,7 +148,7 @@ int main(int argc, char** argv)
   //RooArgSet kinematcVars(massKPi, cosMuMu, cosKstar, phi);
 
   //GooFit
-  Variable massKPi("massKPi",1.,0.6,2.2);
+  Variable massKPi("massKPi",1.,0.7,2.1);
   Variable cosMuMu("cosMuMu",0.,-1,1); // cosine of the psi(nS) helicity angle
   Variable cosKstar("cosKstar",0.,-1,1); // cosine of the K* helicity angle
   Variable phi("phi",0.25,-TMath::Pi(),TMath::Pi());
