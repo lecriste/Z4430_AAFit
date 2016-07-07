774f9b808de0ec53de8b14b8e604d6a514eb76d8
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-07-07 18:18:50 +0200

    v

--- ThreeBodiesPsiPiKPdf.hh
+++ ThreeBodiesPsiPiKPdf.hh
@@ -13,9 +13,9 @@ public:
     ThreeBodiesPsiPiK (std::string n, Variable* _x,Variable* _mp,Variable* _m1,Variable* _m2,Variable* _m3);//,Variable* nBk);
   __host__ fptype integrate (fptype lo, fptype hi) const;
   __host__ virtual bool hasAnalyticIntegral () const {return false;}
-  __global__ void fillHisto (TH1F* histo,unsigned int nevents);
-  __global__ void fillDataset (BinnedDataSet dataset,unsigned int nevents);
-  __global__ void fillDataset (UnbinnedDataSet dataset,unsigned int nevents);
+  //__global__ void fillHisto (TH1F* histo,unsigned int nevents);
+  //__global__ void fillDataset (BinnedDataSet dataset,unsigned int nevents);
+  //__global__ void fillDataset (UnbinnedDataSet dataset,unsigned int nevents);
 
 
 private:
