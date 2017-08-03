bc54939772690269d61978492a33ad868f18c910
surnairit <nairit.sur@cern.ch>
2017-08-03 12:24:36 +0200

    enable options

--- GooFit/AArun.sh
+++ GooFit/AArun.sh
@@ -4,3 +4,5 @@ AmplitudeAnalysis -k892 -z4200 -z4430 -r ../datasets/B0/Kstar_892_1_Zcs___4200_1
 AmplitudeAnalysis -k892 -k800 -k1410 -k1430_0 -k1430_2 -k1780 -z4200 -z4430 -r ../datasets/B0/Kstars__892_1__800_0__1410_1__1430_0__1430_2__1780_3_Zcs___4200_1__4430_1__cutBased.txt
 AmplitudeAnalysis -z4200 -z4430 -r ../datasets/B0/_Zcs___4200_1__4430_1__cutBased.txt
 AmplitudeAnalysis -k892 -k1430_2 -r ../datasets/B0/Kstars__892_1__1430_2__cutBased_100Kevents.txt
+AmplitudeAnalysis -k892 -k800 -k1410 -k1430_0 -k1430_2 -k1780 -z4200 -z4430 -r ../datasets/B0/Kstars__892_1__800_0__1410_1__1430_0__1430_2__1780_3_Zcs___4200_1__4430_1__cutBased_parityCorrected.txt
+AmplitudeAnalysis -k892 -z4200 -z4430 -r ../datasets/B0/Kstar_892_1_Zcs___4200_1__4430_1__cutBased_parityCorrected.txt
