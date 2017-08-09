8d0813fc80d8ae55bb812d88a07e925db8d298c7
surnairit <nairit.sur@cern.ch>
2017-08-09 17:01:16 +0200

    colour scheme change

--- GooFit/AArun.sh
+++ GooFit/AArun.sh
@@ -6,3 +6,10 @@ AmplitudeAnalysis -z4200 -z4430 -r ../datasets/B0/_Zcs___4200_1__4430_1__cutBase
 AmplitudeAnalysis -k892 -k1430_2 -r ../datasets/B0/Kstars__892_1__1430_2__cutBased_100Kevents.txt
 AmplitudeAnalysis -k892 -k800 -k1410 -k1430_0 -k1430_2 -k1780 -z4200 -z4430 -r ../datasets/B0/Kstars__892_1__800_0__1410_1__1430_0__1430_2__1780_3_Zcs___4200_1__4430_1__cutBased_parityCorrected.txt
 AmplitudeAnalysis -k892 -z4200 -z4430 -r ../datasets/B0/Kstar_892_1_Zcs___4200_1__4430_1__cutBased_parityCorrected.txt
+AmplitudeAnalysis -k892 -k800 -k1410 -k1430_0 -k1430_2 -k1680 -k1780 -k1950 -k1980 -k2045 -z4200 -z4430 -r ../datasets/B0/Kstars__892_1__800_0__1410_1__1430_0__1430_2__1680_1__1780_3__1950_0__1980_2__2045_4_Zcs___4200_1__4430_1__cutBased.txt
+
+AmplitudeAnalysis -k892 -k800 -k1410 -k1430_0 -k1430_2 -k1780 -z4200 -z4430 -r ../datasets/B0bar/Kstars__892_1__800_0__1410_1__1430_0__1430_2__1780_3_Zcs___4200_1__4430_1__cutBased.txt
+
+AmplitudeAnalysis -k892 -k800 -k1410 -k1430_0 -k1430_2 -k1780 -r ../datasets/B0/Kstars__892_1__800_0__1410_1__1430_0__1430_2__1780_3__cutBased_parityCorrected.txt
+
+AmplitudeAnalysis -k892 -k800 -k1410 -k1430_0 -k1430_2 -k1680 -k1780 -k1950 -k1980 -k2045 -r ../datasets/B0/Kstars__892_1__800_0__1410_1__1430_0__1430_2__1680_1__1780_3__1950_0__1980_2__2045_4__cutBased_parityCorrected.txt
