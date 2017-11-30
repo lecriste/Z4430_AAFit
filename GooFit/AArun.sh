31884171abe8c2673c89e22f94e10b8bc0d4df20
Leonardo Cristella <leonardo.cristella@cern.ch>
2017-11-30 20:07:54 +0100

    Revert "Z included in gooFit"
    This reverts commit 818cd7d64b32d653a19b4b6f0c84b9eccb1f6dfc.

--- GooFit/AArun.sh
+++
@@ -1,6 +0,0 @@
-AmplitudeAnalysis -k892 -z4200 -z4430 -r ../datasets/B0/Kstar_892_1_Zcs___4200_1__4430_1__cutBased.txt
-AmplitudeAnalysis -k892 -r ../datasets/B0/Kstar_892_1_Zcs___4200_1__4430_1__cutBased.txt 
-AmplitudeAnalysis -k892 -z4200 -z4430 -r ../datasets/B0/Kstar_892_1_Zcs___4200_1__4430_1__cutBased_100Kevents.txt
-AmplitudeAnalysis -k892 -k800 -k1410 -k1430_0 -k1430_2 -k1780 -z4200 -z4430 -r ../datasets/B0/Kstars__892_1__800_0__1410_1__1430_0__1430_2__1780_3_Zcs___4200_1__4430_1__cutBased.txt
-AmplitudeAnalysis -z4200 -z4430 -r ../datasets/B0/_Zcs___4200_1__4430_1__cutBased.txt
-AmplitudeAnalysis -k892 -k1430_2 -r ../datasets/B0/Kstars__892_1__1430_2__cutBased_100Kevents.txt
